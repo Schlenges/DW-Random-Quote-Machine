07c3b08324a8121dbddeb33519a64775692982b3
Meike <meike_hankewicz@yahoo.de>
2018-05-15 15:02:39 +0200

    updated comments & use of jquery syntax

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 var api = "https://dw-quote-api.herokuapp.com/quotes";
 
 // Generate Quote on Button Click
-document.getElementById('getQuote').onclick = function(){
+$("#getQuote").click(function(){
     // AJAX Call
     $.getJSON(api, function(data){
         quote = data[0].quote;
         character = data[0].character;
-        // Display Quote
-        document.getElementById('quote').innerHTML = "<i class='fa fa-angle-double-right' aria-hidden='true'></i>"
-                                                        +quote+
-                                                     "<i class='fa fa-angle-double-left' aria-hidden='true'></i></br>"
-                                                      + character;
 
-        // Check If Tweet Button Exists
-        if(!document.getElementById('tweet')){
+        // Display Quote
+        $("#quote").html("<i class='fa fa-angle-double-right' aria-hidden='true'></i>" + quote + "<i class='fa fa-angle-double-left' aria-hidden='true'></i></br>" + character);
+        
+        // Show Tweet Button
+        if(!$("#tweet").length){
             addTweetBtn();
         }  
     });
-   
-};
+});
 
 // Generate Tweet Button
 function addTweetBtn(){
     var img = document.createElement("img");
     img.id = "tweet";
     img.src = "https://png.icons8.com/sonic-screwdriver/office/50/000000"
-    document.getElementById('buttons').appendChild(img);
+    $("#buttons").append(img);
     tweetQuote();
 };
 
-// Add Tweet Function
+// Add Tweet Functionality
 function tweetQuote(){
-    document.getElementById('tweet').onclick = function(){
+    $("#tweet").click(function(){
         var encodedQuote = encodeURI(quote);
         var encodedCharacter = encodeURI(character);
         var url = 'https://twitter.com/intent/tweet?text="' + encodedQuote + '" - ' + encodedCharacter;
+        
         window.open(url, "Tweet Quote", "resizable, height=360, width=500");
-    };
+    });
 };
